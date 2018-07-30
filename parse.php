f02f16e56b73f62e142cf0648d38e233eee5f3dd
Clemens Basler <clemens-basler@t-online.de>
2018-07-30 14:21:39 +0200

    testing out

---
+++ parse.php
@@ -0,0 +1,14 @@
+<?php
+$ini = parse_ini_file('config.ini');
+
+
+echo $ini['attributes'][0] . "\n";
+echo $ini['attributes'][1] . "\n";
+echo $ini['apple'] . "\n";
+
+$test = parse_ini_file('config.ini', true);
+
+echo $test['fruitCounter']['apple'] . "\n";
+
+var_dump($test);
+ ?>
