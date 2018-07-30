c82bcce21f23b4b20ba6d68645ee2db7b8f7babc
Clemens Basler <clemens-basler@t-online.de>
2018-07-30 16:02:29 +0200

    trying araound

--- parse.php
+++ parse.php
@@ -1,7 +1,6 @@
 <?php
 $ini = parse_ini_file('config.ini');
 
-
 echo $ini['attributes'][0] . "\n";
 echo $ini['attributes'][1] . "\n";
 echo $ini['apple'] . "\n";
@@ -9,6 +8,9 @@ echo $ini['apple'] . "\n";
 $test = parse_ini_file('config.ini', true);
 
 echo $test['fruitCounter']['apple'] . "\n";
+echo $ini['animals'] . "\n";
 
 var_dump($test);
+
+// $test = file($filePath, )
  ?>
